20bc84277b31133b2b00620cb4b6bd55df47b372
kc <[email protected]>
2025-07-21 21:58:30 -0700

    figuring out path logic for elevator

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -15,32 +15,47 @@ Controller::Controller(int lowestFloor, int highestFloor) {
     this->floorOffset = -lowestFloor;
 
     for (int i=0; i<=highestFloor+floorOffset; ++i) {
-        this->floorButtons.push_back(ElevatorButton(i-floorOffset));
+        floorButtons.push_back(ElevatorButton(i-floorOffset));
     };
 }
 
 
-void Controller::setCurrentFloor(int currFloor) {
-    if (currFloor >= this->lowestFloor && currFloor <= this->highestFloor) {
-        this->currentFloor = currFloor;
+void Controller::setCurrentFloor(int currentFloor) {
+    int actualFloor = currentFloor + floorOffset;
+
+    if (currentFloor >= lowestFloor && currentFloor <= highestFloor) {
+        
+        this->currentFloor = currentFloor;
         
         // TODO: remove first element in the elevator path
+        elevatorPath.erase(elevatorPath.begin());
         
-        // TODO: re sort the elevator path 
+        // TODO: re sort the elevator path
+        handlePathLogic();
+
+        floorButtons[actualFloor].setPress(false);
+
     } else {
         throw std::invalid_argument("Invalid floor");
     };
 
-    std::cout << "The current floor is: " << this->currentFloor << '\n';
+    std::cout << "The current floor is: " << floorButtons[actualFloor].getFloorNumber() << '\n';
 }
 
 
 void Controller::handleButtonPress(int floor) {
-    if (floor >= this->lowestFloor && floor <= this->highestFloor) {
-        this->elevatorPath.push_back(floor);
+    if (floor >= lowestFloor && floor <= highestFloor) {
+        // TODO: need to handle initial press to set direction 
+        if (elevatorPath.empty()) {
+            if (floor < currentFloor) {
+                setDirection(false);
+            } 
+        }
+
+        elevatorPath.push_back(floor);
         
         // TODO: setting button active
-        this->floorButtons[floor+floorOffset].setPress(true);
+        floorButtons[floor+floorOffset].setPress(true);
 
     } else {
         throw std::invalid_argument("Invalid floor");
@@ -50,13 +65,23 @@ void Controller::handleButtonPress(int floor) {
 }
 
 void Controller::handlePathLogic() {
-    // TODO: handlePathLogic
-    // is it better to have a deque?
-    //      - can pop front
-    //      - but would have to sort the vector before that
+    // no need to swap
+    // just use a pointer = currFloor and move along the vector until the list is empty
 
-    // or is it better to use a vector?
-    //      - worse time for removing front item after sorting
+    // TODO: sort the list first
+
+    // TODO: find the curr floor in the vector
+
+    // TODO: depending on direction move up or down the array while removing items
+    
+    // TODO: once reaching beginning or end, switch directions until vector is empty
+    
+
+}
+
+// basic sorting method
+void sortVector() {
+
 }
 
 /* 
